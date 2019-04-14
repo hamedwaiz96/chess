eca58899307b5c284b57413c570fa4a95032c4f7
Hamed Waiz <hamedw96@mgmail.com>
2019-04-13 21:15:48 -0700

    phase 1 complete

---
+++ lib/Chess/board.rb
@@ -0,0 +1,46 @@
+require_relative "piece"
+
+class Board
+	def initialize
+		rows = Array.new(8) {Array.new(8)}
+		@rows = rows
+		x = 0
+		k = 0
+		while x <= 7
+			while k <= 7
+				if x == 0 || x == 1 || x == 6 || x == 7
+					@rows[x][k] = Piece.new
+				else
+					@rows[x][k] = nil
+				end
+				k += 1
+			end
+			k = 0
+			x += 1
+		end
+		@sentinel = NullPiece.new
+	end
+
+	def [](pos)
+		row, col = pos
+		@rows[row][col]
+	end
+
+	def []=(pos, value)
+		row, col = pos
+		@rows[row][col] = value
+	end
+
+	def move_piece(start_pos, end_pos)
+		row1, col1 = start_pos
+		row2, col2 = end_pos
+		raise ArgumentError.new("no piece at start") if @rows[row1][col1] == nil
+		raise ArgumentError.new("end_pos has piece on it") if @rows[row2][col2].is_a?(Piece) == true
+		a = @rows[row1].delete_at(col1)
+		b = @rows[row2].delete_at(col2)
+		@rows[row1][col1] = b
+		@rows[row2][col2] = a
+	end
+
+end
+
---
+++ lib/Chess/piece.rb
@@ -0,0 +1,9 @@
+class Piece
+
+	
+
+end
+
+class NullPiece < Piece
+
+end
